9db09d53c405a9aeaa86bd94e21372952cb81ca7
Ale Rimoldi <ale@graphicslab.org>
2014-06-30 22:05:00 +0200

    lot of changes for going online with the new site

--- theme/schloff/template.php
+++ theme/schloff/template.php
@@ -9,7 +9,7 @@
 <body>
 <div id="kopf">
 		<img src="<?= get_theme_url(); ?>/bild/banner_<?= get_page_slug(false) ?>.jpg" id="banner" alt="Schlofftheater">
-		<a href="<?= get_site_url(); ?>"><img src="<?= get_theme_url(); ?>/bild/logo_390.png" alt="logo" id="logo"></a>
+		<a href="<?= get_site_url(); ?>"><img src="<?= get_theme_url(); ?>/bild/<?= false && get_page_slug(false) == 'verein' ? 'logo_verein_392.png' : 'logo_390.png' ?>" alt="logo" id="logo<?= false && get_page_slug(false) == 'verein' ? '_verein' : '' ?>"></a>
 
 	<div id="navigation">
 		<ul>
@@ -19,7 +19,63 @@
 </div>
 <div id="hauptteil" style="position: relative;">
 	<div id="inhalt">
-    <?= get_page_content(); ?>
+    <?= false && get_page_slug(false) ?>
+    <?php
+    // this should stay in, as long as projekte and kontakt are not correctly implemented
+    switch (get_page_slug(false)) {
+     case ('projekte') :
+        include_once(GSTHEMESPATH .$TEMPLATE."/".'old/projekte.php');
+     break;
+     case ('kontakt') :
+        // echo "<pre>".print_r($_POST, 1)."</pre>";
+        if (!empty($_POST) && (!empty($_POST['mitteilung']) || !empty($_POST['email']))) {
+
+            // Debugger::structure('_POST', $_POST);
+            /*
+        [name] => ale
+            [adresse] => spatenstrasse 23
+        8050 zÃ¼rich
+            [telefon] => 043 288 50 43
+            [email] => ale@ideale.ch
+            [mitteilung] => test mitteilung
+            [schicken] => schicken
+        )
+        */
+            $content = "";
+            if (!empty($_POST['name'])) {
+                $content .= "Name: ".$_POST['name']."\n\n";
+            }
+            if (!empty($_POST['mitteilung'])) {
+                $content .= "Mitteilung: ".$_POST['mitteilung']."\n\n";
+            }
+            $email = $_POST['email'];
+            if (!empty($_POST['email'])) {
+                $content .= "Email: ".$_POST['email']."\n\n";
+            }
+            if (!empty($_POST['telefon'])) {
+                $content .= "Telefon: ".$_POST['telefon']."\n\n";
+            }
+            if (!empty($_POST['adresse'])) {
+                $content .= "Adresse:\n".$_POST['adresse']."\n\n";
+            }
+            if (array_key_exists('adresse', $_POST)) {
+                $content .= "Mitgliedschaft: ich möchte Mitglied werden\n";
+            }
+            // echo($content);
+            // Debugger::structure('content', $content);
+            if (!empty($content)) {
+                // mail("beatrice.mock@schlofftheater.ch","Kontaktformular",$content,"", (empty($email) ? "" : "-f ".$email));
+                mail("a.l.e@xox.ch","Kontaktformular",$content,"", (!empty($email) && (strpos($mail, '@') !== false) ? "" : "-f ".$email));
+                mail("beatrice.mock@schlofftheater.ch","Kontaktformular",$content,"", (!empty($email) && (strpos($mail, '@') !== false) ? "" : "-f ".$email));
+            }
+            echo("<p><strong>Danke für die Mitteilung</strong></p>\n<pre>".$content."</pre>");
+        }
+        get_page_content();
+     break;
+     default :
+        get_page_content();
+    }
+    ?>
 	</div>
 </div>
 <div id="fusszeile">
